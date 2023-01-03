06ca37ae964e4950134d54ce7ee1f0054db0efb8
Disa-Kizonda <53359732+Disa-Kizonda@users.noreply.github.com>
2023-01-03 22:53:38 +1000

    Now images are splitting

--- start.py
+++ start.py
@@ -2,30 +2,53 @@ import tkinter as tk
 from tkinter import filedialog
 import numpy as np
 from PIL import Image
+import re,struct
 def encode_file():
     filepaths = filedialog.askopenfilenames()
     for filepath in filepaths:
         with open(filepath, "rb") as file:
             file_bytes = file.read()
-        file_bytes = len(file_bytes).to_bytes(4, 'big') + file_bytes
+        file_bytes = struct.pack(">I", len(file_bytes)) + file_bytes
+        num_padding_bytes = 2048 - len(file_bytes) % 2048
+        file_bytes += b"\0" * num_padding_bytes
         lines = [file_bytes[i:i+2048] for i in range(0, len(file_bytes), 2048)]
-        image = np.empty((len(lines), 2048), dtype=np.uint8)
-        for i, line in enumerate(lines):
-            padded_line = np.pad(np.frombuffer(line, dtype=np.uint8), (0, 2048-len(line)), mode="constant")
-            image[i] = padded_line
-        image = Image.fromarray(image, "L")
-        image.save(filepath + ".png")
+        image = np.frombuffer(b"".join(lines), dtype=np.uint8).reshape((len(lines), 2048))
+        images = []
+        for y in range(0, image.shape[0], 2048):
+            for x in range(0, image.shape[1], 2048):
+                new_image = image[y:y+2048, x:x+2048]
+                images.append(new_image)
+        for i, img in enumerate(images):
+            img = Image.fromarray(img, "L")
+            img.save(filepath + "." + str(i) + ".png")
     print("The files have been successfully encoded!")
 def decode_file():
     filepaths = filedialog.askopenfilenames()
     for filepath in filepaths:
-        image = Image.open(filepath)
-        width, height = image.size
-        image_data = np.array(image.getdata(), dtype=np.uint8)
+        if not (".0.png" in filepath):
+            continue
+        image_data = None
+        i = 0
+        while True:
+            split_image_path = filepath[:-6] + "." + str(i) + ".png"
+            try:
+                image = Image.open(split_image_path)
+                if image_data is None:
+                    image_data = np.array(image.getdata(), dtype=np.uint8)
+                else:
+                    image_data = np.concatenate((image_data, np.array(image.getdata(), dtype=np.uint8)))
+            except:
+                break
+            i += 1
+        else:
+            image = Image.open(filepath)
+            image_data = np.array(image.getdata(), dtype=np.uint8)
         file_bytes = image_data.tobytes()
         file_size = int.from_bytes(file_bytes[:4], 'big')
         file_bytes = file_bytes[4:file_size+4]
-        with open(filepath[:-4], "wb") as output_file:
+        file_extension = re.search(r"\.[^.]*$", filepath[:-6]).group()
+        output_file_path = filepath[:-6][:-len(file_extension)] + file_extension
+        with open(output_file_path, "wb") as output_file:
             output_file.write(file_bytes)
     print("The files have been successfully decoded!")
 root = tk.Tk()
