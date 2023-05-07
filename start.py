d895a813c87655b58969763db3a3ffb0c2cc5f9f
Disa-Kizonda <53359732+Disa-Kizonda@users.noreply.github.com>
2023-05-07 13:12:08 +1000

    Less code

--- start.py
+++ start.py
@@ -7,37 +7,30 @@ def encode_file():
     filepaths = filedialog.askopenfilenames()
     for filepath in filepaths:
         with open(filepath, "rb") as file:
-            file_bytes = file.read()
-        file_bytes = struct.pack(">Q", len(file_bytes)) + file_bytes
+            data = file.read()
+            file_bytes = struct.pack(">Q", len(data)) + data
         num_padding_bytes = 2048 - len(file_bytes) % 2048
-        file_bytes += b"\0" * num_padding_bytes
-        image = np.frombuffer(file_bytes, dtype=np.uint8).reshape((-1, 2048))
+        image = np.frombuffer(file_bytes + b"\0" * num_padding_bytes, dtype=np.uint8).reshape((-1, 2048))
         for i in range(0, image.shape[0], 2048):
-            img = Image.fromarray(image[i:i+2048], "L")
-            img.save(f"{filepath}.{i//2048}.png")
+            Image.fromarray(image[i:i+2048], "L").save(f"{filepath}.{i//2048}.png")
     print("The files have been successfully encoded!")
 def decode_file():
     filepaths = filedialog.askopenfilenames()
     for filepath in filepaths:
-        if not (".0.png" in filepath):
+        if ".0.png" not in filepath:
             continue
         image_data = []
         i = 0
         while True:
-            split_image_path = filepath[:-6] + "." + str(i) + ".png"
             try:
-                image = Image.open(split_image_path)
-                image_data.append(np.array(image))
+                image_data.append(np.array(Image.open(f"{filepath[:-6]}.{i}.png")))
             except:
                 break
             i += 1
-        image_data = np.concatenate(image_data)
-        file_bytes = image_data.tobytes()
+        file_bytes = np.concatenate(image_data).tobytes()
         file_size = int.from_bytes(file_bytes[:8], 'big')
-        file_bytes = file_bytes[8:file_size+8]
-        output_file_path = re.sub(r"\.\d+\.png$", "", filepath)
-        with open(output_file_path, "wb") as output_file:
-            output_file.write(file_bytes)
+        with open(re.sub(r"\.\d+\.png$", "", filepath), "wb") as output_file:
+            output_file.write(file_bytes[8:file_size+8])
     print("The files have been successfully decoded!")
 root = tk.Tk()
 root.title("Encoding/decoding a file")
