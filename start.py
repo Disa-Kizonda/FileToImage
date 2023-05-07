e1f2f27a1d43f9a2b6a922741d98618416ddceca
Disa-Kizonda <53359732+Disa-Kizonda@users.noreply.github.com>
2023-05-07 12:05:35 +1000

    Less code

--- start.py
+++ start.py
@@ -11,38 +11,27 @@ def encode_file():
         file_bytes = struct.pack(">Q", len(file_bytes)) + file_bytes
         num_padding_bytes = 2048 - len(file_bytes) % 2048
         file_bytes += b"\0" * num_padding_bytes
-        lines = [file_bytes[i:i+2048] for i in range(0, len(file_bytes), 2048)]
-        image = np.frombuffer(b"".join(lines), dtype=np.uint8).reshape((len(lines), 2048))
-        images = []
-        for y in range(0, image.shape[0], 2048):
-            for x in range(0, image.shape[1], 2048):
-                new_image = image[y:y+2048, x:x+2048]
-                images.append(new_image)
-        for i, img in enumerate(images):
-            img = Image.fromarray(img, "L")
-            img.save(filepath + "." + str(i) + ".png")
+        image = np.frombuffer(file_bytes, dtype=np.uint8).reshape((-1, 2048))
+        for i in range(0, image.shape[0], 2048):
+            img = Image.fromarray(image[i:i+2048], "L")
+            img.save(f"{filepath}.{i//2048}.png")
     print("The files have been successfully encoded!")
 def decode_file():
     filepaths = filedialog.askopenfilenames()
     for filepath in filepaths:
         if not (".0.png" in filepath):
             continue
-        image_data = None
+        image_data = []
         i = 0
         while True:
             split_image_path = filepath[:-6] + "." + str(i) + ".png"
             try:
                 image = Image.open(split_image_path)
-                if image_data is None:
-                    image_data = np.array(image.getdata(), dtype=np.uint8)
-                else:
-                    image_data = np.concatenate((image_data, np.array(image.getdata(), dtype=np.uint8)))
+                image_data.append(np.array(image))
             except:
                 break
             i += 1
-        else:
-            image = Image.open(filepath)
-            image_data = np.array(image.getdata(), dtype=np.uint8)
+        image_data = np.concatenate(image_data)
         file_bytes = image_data.tobytes()
         file_size = int.from_bytes(file_bytes[:8], 'big')
         file_bytes = file_bytes[8:file_size+8]
